a804d885b5af66d7a2dfd4f63077ba03f6b9c9f8
josephatla <andifajose@gmail.com>
2025-12-22 18:09:00 +0700

    Deploy FIx

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,17 +4,24 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+return Application::configure(
+    basePath: realpath(__DIR__ . '/..')
+)
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
-        $middleware->web(append: [\App\Http\Middleware\SetLocale::class,]);
-        $middleware->alias(['premium' => \App\Http\Middleware\PremiumMiddleware::class,]);
+        $middleware->web(append: [
+            \App\Http\Middleware\SetLocale::class,
+        ]);
+
+        $middleware->alias([
+            'premium' => \App\Http\Middleware\PremiumMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->create();
