c00b9e50e14ff66aef1205fd874acf9134dd6a56
josephatla <andifajose@gmail.com>
2025-12-22 18:43:53 +0700

    Fix: Point Laravel caches to /tmp for Vercel

--- api/index.php
+++ api/index.php
@@ -1,5 +1,16 @@
 <?php
 
+// 1. Move to the project root
 chdir(__DIR__ . '/..');
 
+// 2. Point all Laravel internal caches to /tmp (the only writable directory on Vercel)
+$writableDir = '/tmp';
+
+putenv("APP_PACKAGES_CACHE={$writableDir}/packages.php");
+putenv("APP_SERVICES_CACHE={$writableDir}/services.php");
+putenv("APP_CONFIG_CACHE={$writableDir}/config.php");
+putenv("APP_ROUTES_CACHE={$writableDir}/routes-v7.php");
+putenv("APP_EVENTS_CACHE={$writableDir}/events.php");
+
+// 3. Load the standard Laravel entry point
 require __DIR__ . '/../public/index.php';
